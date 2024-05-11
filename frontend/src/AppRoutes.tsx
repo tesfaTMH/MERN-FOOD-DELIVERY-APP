6c8d436c63ec0946b2458f8b4c5570b4c5f97815
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-05-11 20:40:11 +0300

    access token using auth0

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,15 +1,27 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Layout from './Layouts/Layout'
-import { Home } from './pages/Home'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./Layouts/Layout";
+import { Home } from "./pages/Home";
+import AuthCallbackPages from "./pages/AuthCallbackPages";
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Layout><Home /></Layout>} />
-        <Route path='/user-profile' element={<span>User profile page Page</span>} />
-        <Route path='/*' element={<Navigate to='/' />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPages />} />
+      <Route
+        path="/user-profile"
+        element={<span>User profile page Page</span>}
+      />
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
